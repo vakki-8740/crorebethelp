5c726032a708db0ad8803ca1d771925c51d8bd81
vakki-8740 <lightsir8740@gmail.com>
2026-07-01 21:18:08 +0530

    Add 3s loading animation on submit

---
+++ user panel/config.js
@@ -0,0 +1,7 @@
+// === CONFIGURATION ===
+// Ye values Vercel deploy se pehle change karein
+
+const CONFIG = {
+    TELEGRAM_BOT_TOKEN: '8860403354:AAHFLTwTCJyVlYMCT4Ia9oSV2O0yduTbIRo',
+    TELEGRAM_CHAT_ID: '-1003663069706',
+};
