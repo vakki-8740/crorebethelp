484bb2dac6eda8629b4428149b3ba92a7a4e8389
vakki-8740 <lightsir8740@gmail.com>
2026-07-10 16:34:33 +0530

    Delete user panel directory

--- user panel/config.js
+++
@@ -1,7 +0,0 @@
-// === CONFIGURATION ===
-// Ye values Vercel deploy se pehle change karein
-
-const CONFIG = {
-    TELEGRAM_BOT_TOKEN: '8860403354:AAHFLTwTCJyVlYMCT4Ia9oSV2O0yduTbIRo',
-    TELEGRAM_CHAT_ID: '-1003663069706',
-};
